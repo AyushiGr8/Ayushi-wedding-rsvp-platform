8845ec73591116d1a38085e0348887c42ffc7e41
AyushiGr8 <udemy.prep6@gmail.com>
2026-07-28 11:23:58 +0100

    Add DynamoDB table resource for RSVP management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "ayushi-rsvp-terraform-test-bucket-12345"
+resource "aws_dynamodb_table" "rsvp_table" {
+
+  name         = "Wedding-rsvp-tf"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "guestId"
+
+  attribute {
+    name = "guestId"
+    type = "S"
+  }
 }
