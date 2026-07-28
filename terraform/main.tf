32a3f2fdfe88c4823fd9a0b4dbbc8adecb0b5bc8
AyushiGr8 <udemy.prep6@gmail.com>
2026-07-28 11:55:33 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-resource "aws_dynamodb_table" "rsvp_table" {
-
-  name         = "Wedding-rsvp-tf"
-  billing_mode = "PAY_PER_REQUEST"
-
-  hash_key = "guestId"
-
-  attribute {
-    name = "guestId"
-    type = "S"
-  }
-}
 resource "aws_s3_bucket" "frontend_bucket" {
-
-  bucket = "ayushi-rsvp-tf-frontend-bucket"
+  bucket = "ayushi-rsvp-frontend-tf-unique-name"
 }
